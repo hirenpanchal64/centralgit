63529ff529eff4a37e5af9ffc22ec216d097f0bf
hiren.panchal <hiren.panchal@forcepointon.xyz>
2020-07-21 20:27:15 +0000

    first-commit

---
+++ esgshell_import.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -L -o /tmp/esgshell https://drive.google.com/uc?id=1u2n16wcRhvdMtcvNSfXqeGOGKMTjXfEo&export=download
+sleep 3
+cp /tmp/esgshell /usr/local/sbin/
+chmod 777 /usr/local/sbin/esgshell
+echo "esgshell has been imported successfully..!!"
---
+++ esgshell_remove.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf /tmp/esgshell
+rm -rf /usr/local/sbin/esgshell
+echo "esgshell has been successfully removed..!!"
