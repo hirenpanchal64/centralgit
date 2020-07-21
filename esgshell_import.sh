221ea5dcc70f14a6618fbf6116496371ab4afc68
hiren.panchal <hiren.panchal@forcepointon.xyz>
2020-07-21 20:35:35 +0000

    second-commit

--- esgshell_import.sh
+++ esgshell_import.sh
@@ -4,4 +4,4 @@ curl -L -o /tmp/esgshell https://drive.google.com/uc?id=1u2n16wcRhvdMtcvNSfXqeGO
 sleep 3
 cp /tmp/esgshell /usr/local/sbin/
 chmod 777 /usr/local/sbin/esgshell
-echo "esgshell has been imported successfully..!!"
+echo "esgshell has been imported successfully..!!!"
